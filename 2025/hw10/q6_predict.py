7a44b19f7232d07054fb69f9eee7b17d7a760837
Piotr Kitłowski <piotr.kitlowski@gmail.com>
2025-12-21 22:31:32 +0000

    Final hw10

---
+++ 2025/hw10/q6_predict.py
@@ -0,0 +1,25 @@
+import pickle
+import uvicorn # type: ignore
+from fastapi import FastAPI # type: ignore
+
+
+app = FastAPI(title='lead-scoring')
+
+with open('pipeline_v2.bin', 'rb') as f_in:
+    pipeline = pickle.load(f_in)
+
+@app.post('/predict')
+def predict(lead: dict) -> dict:
+    y_pred = pipeline.predict_proba(lead)[0, 1]
+    conversion = y_pred >= 0.5
+
+    result = {
+        'conversion_probability': float(y_pred),
+        'conversion': bool(conversion)
+    }
+
+    return result
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9696)
